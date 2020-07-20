f749c57e43fded5d9bd6604db27696490ae71b26
zhouchao <chao.zhou@raykite.com>
2020-07-20 19:40:37 +0800

    创建注册中心和order模块

---
+++ spring-cloud-eureka-8080/src/main/java/com/zhouchao/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.zhouchao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author zhouchao
+ * @Date 2020/7/20 18:38
+ * @Description
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-order-8081/src/test/java/com/zhouchao/spring/cloud/order/SpringCloudOrder8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouchao.spring.cloud.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudOrder8081ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
