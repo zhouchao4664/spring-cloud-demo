027c1f1e8ae0c41912a5cc0633febff869d07101
zhouchao <chao.zhou@raykite.com>
2020-09-01 22:26:00 +0800

    创建配置中心

---
+++ spring-cloud-config-server-8084/src/main/java/com/zhouchao/config/server/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.zhouchao.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author zhouchao
+ * @Date 2020/9/1 22:03
+ * @Description
+ **/
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
